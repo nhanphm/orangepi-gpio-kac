d1aa2e618cfa36dc07531f378cdee185b286e88f
Pham Minh Nhan <nhanphm@gmail.com>
2022-09-28 10:38:16 +0700

    Update server.py

--- server.py
+++ server.py
@@ -20,7 +20,7 @@ server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 server.settimeout(0.2)
 message = b"your very important message"
 while True:
-    server.sendto(message, ("localhost", 37020))
+    server.sendto(message, ("192.168.1.255", 37020))
     print("message sent!", flush=True)
     time.sleep(1)
 
