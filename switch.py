4cce8d824a5c54b6ec6fdbb86ad67ab2761a16eb
Pham Minh Nhan <nhanphm@gmail.com>
2022-09-28 14:45:44 +0700

    Update switch.py

--- switch.py
+++ switch.py
@@ -9,7 +9,7 @@ InputPin = 12 #PA07/PA_EINT7/SIM_CLK
 LedOnPin = 13 #PA00/UART2_TX
 
 # Set selected pin to input, need pullup resistor external in 3.3V and pin select.
-GPIO.setup(InputPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(InputPin, GPIO.IN)
 # Set selected pin to output.
 GPIO.setup(LedOnPin, GPIO.OUT)
 
@@ -19,10 +19,13 @@ if GPIO.input(InputPin):
     print('Input was HIGH')
 else:
     print('Input was LOW')
+while True:
+    # Wait for a button press on the selected pin (pin pulled to ground, falling edge)
+    GPIO.wait_for_edge(InputPin, GPIO.BOTH)
+    if GPIO.input(InputPin):
+        print('Input was switch to HIGH')
+    else:
+        print('Input was switch LOW')
+        GPIO.output(LedOnPin, 0)
 
-# Wait for a button press on the selected pin (pin pulled to ground, falling edge)
-GPIO.wait_for_edge(InputPin, GPIO.FALLING)
-
-GPIO.output(LedOnPin, 0)
-
-print ("*** Netctl shutdown activated ***")
+        print ("*** Netctl shutdown activated ***")
